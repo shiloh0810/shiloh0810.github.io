2ec61fe0c8546359a94c6c7ef795dc1e09f75fd3
shiloh0810 <shilohliu@ShilohdeMacBook-Pro.local>
2017-05-06 20:32:50 +0800

    fake info

--- js/db.js
+++ js/db.js
@@ -1,3 +1,22 @@
 var fdb = new ForerunnerDB();
 var db = fdb.db("myDB");
+
 var studentCollection = db.collection('students');
+
+// for (var i = 0; i < 10; i++) {
+// 	var newStudent = {
+// 		name: "Orange" + i,
+// 		age: 10 + i,
+// 	}
+// 	studentCollection.insert(newStudent);
+// }
+// studentCollection.save();
+
+studentCollection.load();
+
+function afterload() {
+    var students = studentCollection.find();
+    console.log(students);
+}
+
+setTimeout(afterload, 1000);
