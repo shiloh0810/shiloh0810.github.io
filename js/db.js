5e3c334cf703a512f2ebf84c4974e7c59845b627
shiloh0810 <shilohliu@ShilohdeMacBook-Pro.local>
2017-05-13 20:28:48 +0800

    modal with information

--- js/db.js
+++ js/db.js
@@ -18,9 +18,19 @@ function afterload() {
     var students = studentCollection.find();
     console.log(students);
     for (var i = 0; i < students.length; i++) {
-    	console.log(students[i]._id);
-    	$("#studentsTable").append("<tr><td>"+i+"</td><td>"+students[i].name+"</td></tr>")
+        $("#studentsTable").append("<tr><td class='studentsId'>" + students[i]._id + "</td><td>" + students[i].name + "</td></tr>")
     }
+    $(".studentsId").click(function() {
+        var studentId = $(this).text();
+        var query = {
+            _id: studentId
+        };
+        var result = studentCollection.find(query)[0];
+        $("#modal-top").text(result._id);
+        $("#inside-name").text(result.name);
+        $("#inside-age").text(result.age);
+        $("#studentsInfo").modal('show');
+    })
 }
 
 setTimeout(afterload, 1000);
