fe4c575fea71a9772e2ed6ae2398cdbde3dc752e
shiloh0810 <shiloh20040810@gmail.com>
2017-06-24 20:11:54 +0800

    criteria

--- js/db.js
+++ js/db.js
@@ -14,15 +14,15 @@ var studentCollection = db.collection('students');
 
 studentCollection.load();
 
-function createHTMLString(_id, name) {
-    return "<tr><td class='studentsId'>" + _id + "</td><td>" + name + "</td><td><button class='updateButton btn btn-warning' data-id='" + _id + "'>修改</button> <button class='deleteButton btn btn-primary' data-id='" + _id + "' >刪除</button></td></tr>";
+function createHTMLString(_id, name, age) {
+    return "<tr><td class='studentsId'>" + _id + "</td><td>" + name + "</td><td>" + age + "</td><td><button class='updateButton btn btn-warning' data-id='" + _id + "'>修改</button> <button class='deleteButton btn btn-primary' data-id='" + _id + "' >刪除</button></td></tr>";
 }
 
 function afterload() {
     var students = studentCollection.find();
     console.log(students);
     for (var i = 0; i < students.length; i++) {
-        $("#studentsTable").append(createHTMLString(students[i]._id, students[i].name))
+        $("#studentsTable").append(createHTMLString(students[i]._id, students[i].name, students[i].age))
     };
     $("#studentsTable").on("click", ".studentsId", function() {
         var studentId = $(this).text();
@@ -48,7 +48,7 @@ $("#submit").click(function() {
     studentCollection.insert(newStudent);
     studentCollection.save();
     var students = studentCollection.find(newStudent)[0];
-    $("#studentsTable").append(createHTMLString(students._id, students.name));
+    $("#studentsTable").append(createHTMLString(students._id, students.name, students.age));
     $("#name").val("");
     $("#age").val("");
 
@@ -93,5 +93,33 @@ $("#save").click(function() {
     studentCollection.updateById(studentId, newStudent);
     console.log(studentId)
     studentCollection.save();
+    location.reload();
 })
+
+function searchData() {
+    var gt = $("#gt").val();
+    var lt = $("#lt").val();
+    //console.log(gt)
+    var result = studentCollection.find({
+        age: {
+            "$gt": gt/1,
+            "$lt": lt/1
+        }
+    });
+    var students = studentCollection.find();
+    $("#studentsTable").find("tr").remove();
+    //console.log(result)
+    for (var i = 0; i < result.length; i++) {
+        $("#studentsTable").append(createHTMLString(result[i]._id, result[i].name, result[i].age));
+    }
+}
+function showAll(){
+    var students = studentCollection.find();
+    $("#studentsTable").find("tr").remove();
+    for (var i = 0; i < students.length; i++) {
+        $("#studentsTable").append(createHTMLString(students[i]._id, students[i].name, students[i].age));
+    }
+}
+$("#search").on("click", searchData)
+$("#showAll").on("click", showAll)
 setTimeout(afterload, 1000);
