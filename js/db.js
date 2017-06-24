f88e730ae6dbcc3908b8900fc5e8e621b8d85971
shiloh0810 <shiloh20040810@gmail.com>
2017-06-24 12:27:42 +0800

    edit

--- js/db.js
+++ js/db.js
@@ -15,7 +15,7 @@ var studentCollection = db.collection('students');
 studentCollection.load();
 
 function createHTMLString(_id, name) {
-    return "<tr><td class='studentsId'>" + _id + "</td><td>" + name + "</td><td><button class='deleteButton btn btn-primary' data-id='" + _id + "' >刪除</button></td></tr>";
+    return "<tr><td class='studentsId'>" + _id + "</td><td>" + name + "</td><td><button class='updateButton btn btn-warning' data-id='" + _id + "'>修改</button> <button class='deleteButton btn btn-primary' data-id='" + _id + "' >刪除</button></td></tr>";
 }
 
 function afterload() {
@@ -66,5 +66,32 @@ function deleteData() {
     }
 }
 
-$("#studentsTable").on("click", ".deleteButton", deleteData)
+function updateData() {
+    var studentId = $(this).attr("data-id");
+    var query = {
+        _id: studentId,
+    };
+    var result = studentCollection.find(query)[0];
+    $("#updateName").val(result.name);
+    $("#updateAge").val(result.age);
+    $("#updateModal").modal('show');
+
+    $("#save").attr("data-id", studentId);
+}
+
+$("#studentsTable").on("click", ".deleteButton", deleteData);
+$("#studentsTable").on("click", ".updateButton", updateData);
+$("#save").click(function() {
+    var studentId = $(this).attr("data-id");
+    var name = $("#updateName").val();
+    var age = $("#updateAge").val();
+    var newStudent = {
+        name: name,
+        age: age,
+    }
+    $("#updateModal").modal('hide');
+    studentCollection.updateById(studentId, newStudent);
+    console.log(studentId)
+    studentCollection.save();
+})
 setTimeout(afterload, 1000);
